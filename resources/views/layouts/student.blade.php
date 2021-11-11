f56ad890cf20e234b2ca1d8fdb96fa90c0494e1d
ywj <shengling_2005@163.com>
2021-11-11 11:36:27 +0800

    remove student footer

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -68,7 +68,7 @@
             <a class="nav-link {{ request()->segment(2) === 'posts' ? 'active' : null }}" href="{{ url('/student/posts') }}">作业记录</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link {{ request()->segment(2) === 'classmate' ? 'active' : null }}" href="{{ url('/student/classmate') }}" tabindex="-1">作业墙</a>
+            <a class="nav-link {{ request()->segment(2) === 'classmate' ? 'active' : null }}" href="{{ url('/student/classmate?type=same-sclass') }}" tabindex="-1">作业墙</a>
           </li>
           <!-- <li class="nav-item">
             <a class="nav-link {{ request()->segment(2) === 'work' ? 'active' : null }}" href="{{ url('/student/work') }}" tabindex="-1">个人主页</a>
@@ -100,30 +100,9 @@
 
     @yield('content')
     @yield('scripts')
-    <footer class="footer mt-auto py-3 bg-light">
-      <div class="container">
-          <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-              <div class="text-center text-sm text-gray-500 sm:text-left">
-                  <div class="flex">
-                      <svg fill="none" width="16" height="16" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
-                          <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                      </svg>
+    <br>
+    <br>
+    <br>
 
-                      <a href="mailto:shengling_2005@163.com" class="ml-1 underline">
-                          技术支持
-                      </a>
-
-                      <svg fill="none" width="16" height="16" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
-                          <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                      </svg>
-
-                      <a href="" class="ml-1 underline">
-                          幻化生灵 All Rights Reserved
-                      </a>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </footer>
 </body>
 </html>
