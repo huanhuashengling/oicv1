e6d073839964439688fb563b1bd765f0a3b5ca0a
wenjie.you <shengling_2005@163.com>
2022-07-10 15:54:04 +0800

    Update unit.blade.php
    update unit img arrange

--- resources/views/student/open-classroom/unit.blade.php
+++ resources/views/student/open-classroom/unit.blade.php
@@ -2,17 +2,21 @@
 
 @section('content')
 <div class="container">
+    <div class="row row-cols-1 row-cols-md-4 g-2">
     {!! Breadcrumbs::render('unit', $unit) !!}
     @foreach($lessons as $key => $lesson)
-    <div class="panel panel-default col-md-4 text-center" style="margin-left:10px; width: 350px; height: 350px;">
-      <div class="panel-body">
+    <div class="col">
+    <div class="card text-center">
+      <div class="card-body">
         <a href="/student/open-classroom/lesson/{{$lesson->id}}">
         <img src="{{$planetUrl}}" alt="..." class="img-circle"></a>
             <h4>{{$lesson->title}}</h4>
             <p class="text-left">{{$lesson->subtitle}}</p>
       </div>
     </div>
+</div>
     @endforeach
+</div>
 </div>
 @endsection
 
