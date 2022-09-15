6680e62c6b8fa7a6eece0d6e3b6db9842b12f19a
wenjie.you <shengling_2005@163.com>
2022-09-16 07:19:59 +0800

    test

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -213,7 +213,7 @@ class HomeController extends Controller
 
         } else {
           // dd(base64_decode(file_get_contents($realPath)));
-          dd($realPath);
+          // dd($realPath);
           // Image::make(file_get_contents($realPath))
           Image::make(file_get_contents($realPath))
               ->resize(240, 180)->save(public_path('posts/' . $schoolCode . '/') . $uniqid . '_c.png');
